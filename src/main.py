36ef94b7a0c8d487ac6af5828d861a24d50c0ddd
abpa123 <aditya.banwasi@gmail.com>
2026-02-16 16:27:42 -0800

    path

--- src/main.py
+++ src/main.py
@@ -25,79 +25,6 @@ def autonomous_function():
 
     log(("Competition", "competition"), "autonomous_begin")
 
-    """
-    # robot_position.reset(Position(1200, 1185))
-    # reset_heading_to_aim(Position(1480, 1185), FORWARD)
-    # matchload.set(True)
-
-    # wait(1000, MSEC)
-
-    # flap.set(True)
-
-    # wait(1000, MSEC)
-
-    # conveyor.spin(REVERSE, FORWARD, FORWARD)
-
-    # # trigger_mover.move(Position(1480, 1185), FORWARD)
-    # trigger_driver.drive(260)
-    # # trigger_driver.drive(-50)
-    # # wait(100, MSEC)
-    # # trigger_driver.drive(70)
-    # matchload.set(False)
-    # wait(220, MSEC)
-    # matchload.set(True)
-
-    # wait(200, MSEC)
-    # # conveyor.spin(STOP, STOP, STOP)
-    # # flap.set(False)
-
-    # trigger_mover.move(Position(1200, 1200), REVERSE)
-    # conveyor.spin(REVERSE, FORWARD, FORWARD)
-    # matchload.set(False)
-    # trigger_turner.turn(177, FRAME_HEADING_RELATIVE)
-    # conveyor.spin(STOP, STOP, STOP)
-    # flap.set(False)
-
-    # trigger_mover.move(Position(900, 1200), FORWARD)
-    # # trigger_turner.turn(270, FRAME_ABSOLUTE) # do another check
-    # conveyor.spin(REVERSE, FORWARD, FORWARD)
-    """
-
-    """
-    robot_position.reset(Position(1600, 450)) # 1600 -> 1575 according to path.jerry.io
-    reset_heading_to_aim(Position(900, 450), FORWARD)
-    flap.set(True)
-
-    trigger_mover.move(Position(900, 450))
-    wait(200, MSEC)
-    #  trigger_turner.turn(111.801, FRAME_HEADING_RELATIVE)
-    conveyor.spin(REVERSE, FORWARD, FORWARD)
-    trigger_mover.move(Position(1200, 1185), FORWARD)
-    # trigger_turner.turn(90, FRAME_HEADING_RELATIVE)
-    trigger_turner.turn(90, FRAME_ABSOLUTE) # do another check
-
-    matchload.set(True)
-    wait(200, MSEC)
-
-    # trigger_mover.move(Position(1480, 1185), FORWARD)
-    trigger_driver.drive(260)
-    matchload.set(False)
-    wait(220, MSEC)
-    matchload.set(True)
-
-    wait(200, MSEC)
-
-    trigger_mover.move(Position(1200, 1200), REVERSE)
-    conveyor.spin(REVERSE, FORWARD, FORWARD)
-    matchload.set(False)
-    trigger_turner.turn(177, FRAME_HEADING_RELATIVE)
-    conveyor.spin(STOP, STOP, STOP)
-    flap.set(False)
-
-    trigger_mover.move(Position(900, 1200), FORWARD)
-    # trigger_turner.turn(270, FRAME_ABSOLUTE) # do another check
-    conveyor.spin(REVERSE, FORWARD, FORWARD)
-    """
     robot_position.reset(Position(1250, 380))
     reset_heading_to_aim(Position(1250, 1200), FORWARD)
 
@@ -107,15 +34,6 @@ def autonomous_function():
     matchload.set(True)
     conveyor.spin(REVERSE, FORWARD, FORWARD)
 
-    # trigger_mover.move(Position(1200, 450), FORWARD)
-    # trigger_turner.turn(90, FRAME_HEADING_RELATIVE)
-    # trigger_mover.move(Position(1200, 1200), FORWARD)
-
-    # trigger_turner.turn(90, FRAME_HEADING_RELATIVE)
-    # trigger_turner.turn(90, FRAME_ABSOLUTE)
-    # conveyor.spin(REVERSE, FORWARD, FORWARD)
-    # # slow_trigger_driver.drive(243)
-
     trigger_mover.move(Position(1250, 1200), FORWARD)
     # trigger_turner.turn(0, FRAME_ABSOLUTE)
     trigger_turner.turn(90, FRAME_HEADING_RELATIVE)
@@ -123,7 +41,6 @@ def autonomous_function():
 
     trigger_driver.drive_for_time(1000, 20, True, 243)
     trigger_driver.drive(-35)
-    reset_robot_position_and_heading_to_gps()
 
     wait(755, MSEC)
 
